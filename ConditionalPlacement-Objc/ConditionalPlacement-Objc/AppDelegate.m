d640156947759f652c21b1e51f678340f8f9cc7b
chenrie <calhenrie@icloud.com>
2015-02-01 14:04:40 -0700

    conditionalPlacement

--- ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
+++ ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
@@ -16,6 +16,38 @@
             
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    int karl;
+    int leonard;
+    
+    karl = 10;
+    leonard = 7;
+    
+    int karlPlace;
+    int leonardPlace;
+    
+    karlPlace = karl > 7?1:2;
+    leonardPlace=leonard>7?1:2;
+    
+    if(karlPlace == 1 && leonardPlace == 1)
+    {
+        NSLog(@" Both teachers are placed 1");
+    }
+    if(karlPlace ==1)
+    {
+        NSLog  (@" Karl is placed 1 and Leonard is placed 2");
+    }
+    if(leonardPlace==1)
+    {
+        NSLog(@"Leonard is placed 1 and Karl is plced 2");
+    }
+    else(leonardPlace =! 1);
+    {
+        NSLog(@"Karl and Leonard are placed 2");
+    }
+    
+    
+    
 
     // Insert code here...
     
